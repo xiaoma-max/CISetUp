f768b408bb64028bd74bbd57a48062c2c04077e8
xiaoma-max <xiaoma_max@hotmail.com>
2017-05-17 15:22:03 +1000

    Initial Commit

---
+++ CISetUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CISetUp
+//
+//  Created by Max on 17/5/17.
+//  Copyright © 2017 appscore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
